4685928cba3a94c293e5273e324319e42320e0d3
paocastrob <paocastrob@gmail.com>
2019-07-24 16:13:59 +0000

    first commit!

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,19 +7,14 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div className="container">
+				<div className="topPart" />
+
+				<div className="blackBox">
+					<div className="redLight" />
+					<div className="yellowLight" />
+					<div className="greenLight" />
+				</div>
 			</div>
 		);
 	}
